ea87bd206da3279e767bc4d52d2408d5b675180c
sp0ka <70164909+sp0ka@users.noreply.github.com>
2020-08-24 20:14:24 -0400

    Add files via upload
    found a way to call the temperature units from the API to eliminate some lines of code. added a precipitation likelihood but it seems a little off.

--- newday.py
+++ newday.py
@@ -1,28 +1,25 @@
 import requests, json, math
 api_key = "8305f8b53083f1201af409b013409f95"
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-city_id = "5110302"
-complete_url = base_url + "id=" + city_id + "&appid=" + api_key
+base_url = "http://api.openweathermap.org/data/2.5/onecall?"
+lat = "40.701780"
+lon = "-73.930750"
+complete_url = base_url + "lat=" + lat + "&lon=" + lon + "&exclude=minutely&units=imperial&appid=" + api_key
 response = requests.get(complete_url)
 x = response.json()
-y = x["main"]
-city_name = x["name"]
-current_temperature = y["temp"]
-feels_like = y["feels_like"]
+y = x["current"]
+z = x["daily"]
+city_name = "Bushwick"
+current_temperature = round(y["temp"])
+feels_like = round(y["feels_like"])
 current_humidity = y["humidity"]
-z = x["weather"]
-weather_description = z[0]["description"]
-celsius_temp = current_temperature - 273.15
-fahrenheit_temp = round(celsius_temp * (9/5) + 32)
-celsius_feels = feels_like - 273.15
-fahrenheit_feels = round(celsius_feels * (9/5) + 32)
+chances_rain = round((z[4]["pop"]) * 100)
 
 print ( str(city_name) +
-            "\n Temperature (fahrenheit) = " +
-                str(fahrenheit_temp) +
+            "\n Temperature (F) = " +
+                str(current_temperature) +
             "\n Feels like = " +
-                str(fahrenheit_feels) +
+                str(feels_like) +
             "\n Humidity = " +
                 str(current_humidity) + '%' +
-            "\n Description = " +
-                str(weather_description))
+            "\n Chance of Rain = " +
+                str(chances_rain) + "%" )
